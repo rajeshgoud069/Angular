6bef1cc6d7a31f14f52bd4df832bdb13f50a74a2
rajeshgoud069 <rajeshgoud069@gmail.com>
2023-04-21 13:26:33 +0100

    Property card added

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  Property:any = {
+    "Id":1,
+    "Type": "House",
+    "Price":1200
+  }
+
+}
